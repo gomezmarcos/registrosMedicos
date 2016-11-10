22fdd13e9197c7179c19d247c413e4666f11820f
Marcos Gomez <mgomez@acquisio.com>
2016-11-10 13:16:30 -0300

    para tyc se necesita el tag de token

--- resources/views/main/tyc/index.blade.php
+++ resources/views/main/tyc/index.blade.php
@@ -5,6 +5,7 @@
     <div class="row">
     <!--<form class="form-horizontal" role="form" method="POST" action="{{ url('/tyc/store') }}">-->
     <form class="form-horizontal" role="form" method="POST" action="/tyc/store">
+        {{ csrf_field() }}
         hola hola
         <input type="submit" value="Aceptar"/>
     </form>
